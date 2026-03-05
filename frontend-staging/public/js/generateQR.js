b9d5d87d7a4478e0a53ac87f02f574db728c3855
crow <crow004@protonmail.com>
2026-03-05 10:18:06 +0400

    Delete frontend-staging directory

--- frontend-staging/public/js/generateQR.js
+++
@@ -1,5 +0,0 @@
-const QRCode = require('qrcode');
-QRCode.toFile('./official_links_qr.png', 'https://muschairs.com/links.html', { width: 300 }, err => {
-  if (err) throw err;
-  console.log('official_links_qr.png created!');
-});
